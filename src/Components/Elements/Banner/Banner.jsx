6c42f428624049a2d59ec57cad6e15ef600a3e6e
choyrulh <choirul.humam@students.amikom.ac.id>
2024-01-09 01:14:55 +0700

    update responsive banner

--- src/Components/Elements/Banner/Banner.jsx
+++ src/Components/Elements/Banner/Banner.jsx
@@ -23,36 +23,28 @@ export function Banner({ label }) {
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.6 }}
-            className="bg-gradient-to-r from-black from-[30%] to-transparent max-h-[80vh] group flex flex-row-reverse justify-start items-start rounded-3xl border"
+            className="relative bg-gradient-to-r from-black from-[30%] to-transparent max-h-[80vh] group flex flex-row-reverse justify-start items-start rounded-3xl border overflow-hidden"
           >
             <img
               src={`https://image.tmdb.org/t/p/original${item.poster_path}`}
-              className="h-[80vh] w-[80%] rounded-3xl object-center mix-blend-overlay"
+              alt={item.title || item.name}
+              className="h-[80vh] w-[80%] sm:w-[70%] md:w-[60%] lg:w-[50%] rounded-3xl object-cover md:object-cover"
             />
             <motion.div
               initial={{ opacity: 0, x: -20 }}
               animate={{ opacity: 1, x: 0 }}
               transition={{ duration: 0.5 }}
-              className="h-[80vh] w-[40%] relative left-0"
+              className="absolute top-0 left-0 h-full w-full sm:w-[30%] md:w-[40%] lg:w-[50%] bg-gradient-to-l from-transparent to-black p-8 text-white"
             >
-              <div className="h-fit w-fit mt-[35%] ml-11">
-                <h2 className="text-5xl">
-                  {item.title ? item.title : item.name}
-                </h2>
-                <h3 className="text-base mt-2">
-                  {`Release Date : ${
-                    item.release_date ? item.release_date : item.first_air_date
-                  }`}
-                </h3>
-                <h4 className="mt-1">
-                  {item.popularity.toLocaleString("id-ID", {
-                    styles: "currency",
-                    currency: "IDR",
-                  })}
-                </h4>
-                <h5 className="text-sm mt-1">
-                  Synopsis:<p>{item.overview.slice(0, 200) + " " + "..."}</p>
-                </h5>
+              <div className="h-full flex flex-col justify-center ">
+                <h2 className="text-4xl mb-2">{item.title || item.name}</h2>
+                <p className="text-sm mb-4">
+                  Release Date: {item.release_date || item.first_air_date}
+                </p>
+                <p className="mb-4">Popularity: {item.popularity}</p>
+                <p className="text-sm">
+                  Synopsis: {item.overview.slice(0, 200) + "..."}
+                </p>
               </div>
             </motion.div>
           </motion.div>
